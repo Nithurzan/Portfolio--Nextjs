8cf641c284e7567d346cf9ef8494594840abbba6
Nithurzan <yogalingam.nithurzan@gmail.com>
2024-12-25 21:33:37 +0530

    update next.config.js file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true,  // Ignore ESLint during the build process
+    },
+  };
+  
+  export default nextConfig;
+  
